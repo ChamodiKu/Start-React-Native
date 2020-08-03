d7167379043fd8505e815a731c96b71716a41af4
ChamodiKu <kulathungachamodi@gmail.com>
2020-08-03 23:32:55 +0530

    Add images use building reusable components

--- App.js
+++ App.js
@@ -3,9 +3,10 @@
 import {createAppContainer} from 'react-navigation'; 
 import {createStackNavigator} from 'react-navigation-stack'; 
 
-import HomeScreen from './src/Screens/HomeScreen';
+import HomeScreen from './src/Screens/HomeScreen';      // . use for go inner folder or file
 import ComponentScreen from './src/Screens/ComponentScreen';
 import ListScreen from './src/Screens/ListScreen';
+import ImageScreen from './src/Screens/ImageScreen';
 
 //little object that decide what content show in the screen at any given poin of time
 //allow us to navigate or essentially change the content that is visible on the screen to users
@@ -13,7 +14,8 @@ const navigator = createStackNavigator (
   {
     Home : HomeScreen,
     Components : ComponentScreen,
-    List: ListScreen
+    List: ListScreen,
+    Image: ImageScreen
   },
 
   {
--- src/Screens/ImageScreen.js
+++ src/Screens/ImageScreen.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-import ImageDetails from '../Components/ImageDetails';
+import ImageDetails from '../Components/ImageDetails'; //.. use for go outer folder or file
 
 const ImageScreen = () => {
     return(
         <View>
-            <ImageDetails title= "Forest"/>
-            <ImageDetails title= "Beach"/>
-            <ImageDetails title= "Mountain"/>
-        </View>
-    );
-};
+            
+            <Text style = {styles.textStyle} >Image Screen</Text>
+            <ImageDetails 
+                title = "Forest" 
+                imageSource = {require('../../assets/forest.jpg')}
+                imageScore = {9}
+            />
+            <ImageDetails 
+                title = "Beach"
+                imageSource = {require('../../assets/beach.jpg')}
+                imageScore = {7}
+            />
+            <ImageDetails 
+                title = "Mountain"
+                imageSource = {require('../../assets/mountain.jpg')}
+                imageScore = {4}
+            />
 
-const styles = StyleSheet.create ({});
+        </View>
+    ); // propes inside ImageDetails tag we can create by our own 
+};  
+//Assign a number to a prop it has to be assign inside of a set of curly braces
+
+const styles = StyleSheet.create ({
+    textStyle:{
+        fontSize: 35
+    }
+});
 
 export default ImageScreen;
